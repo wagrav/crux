613e6b95453133969cfdb5e7d9bdda2170a11701
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-07-14 10:04:59 +0200

    Added ability to run tests

---
+++ modules/bats/run_all_tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function runModuleTests(){
+ local here=$(pwd)
+ local module=$1
+ local test_folder=$2
+ printf "\n# Running tests for module $module\n"
+ cd ../$module && bats -o "$test_folder" -F junit *.bats
+ cd "$here"
+}
+
+function runAll(){
+  result_folder=$(pwd)/tmp
+  mkdir -p "$result_folder"
+  runModuleTests junit "$result_folder"
+}
+
+runAll
