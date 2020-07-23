009e05333d593ae886270bbc92fffd2af1fc36d7
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-07-23 12:21:19 +0200

    Added createConnection step

--- pipelines/azure/arm/create_service_connection.sh
+++ pipelines/azure/arm/create_service_connection.sh
@@ -7,15 +7,16 @@ create_service_connection() {
   local name=$5
   local cluster_name=$6
   local resource_group=$7
+  local path=$8
 
   url=$(az aks show --name $cluster_name --resource-group $resource_group | jq '.fqdn' | sed "s/\"//g") || :
   printf "Creating kubernetes connection $name for cluster: $cluster_name \n\t url: $url"
-  source template.json.sh $name $url $cluster_name $resource_group> payload.json
+  source "$path"/template.json.sh $name $url $cluster_name $resource_group> "$path"/payload.json
   ls
   echo "Sending payload"
-  cat payload.json
+  cat "$path"/payload.json
   #http_code=$(curl -s -o /dev/null -w "%{http_code}" --user $user:$pat -X POST -H "Content-Type: application/json" --data-binary  @$path/payload.json https://dev.azure.com/$org/$project/_apis/serviceendpoint/endpoints?api-version=5.0-preview.2)
-  curl --user $user:$pat -X POST -H "Content-Type: application/json" --data-binary  @payload.json https://dev.azure.com/$org/$project/_apis/serviceendpoint/endpoints?api-version=5.0-preview.2
+  curl --user $user:$pat -X POST -H "Content-Type: application/json" --data-binary  @"$path"/payload.json https://dev.azure.com/$org/$project/_apis/serviceendpoint/endpoints?api-version=5.0-preview.2
   echo "Http code: $http_code"
   if [ "$http_code" != "200" ]; then
     echo "Connection $name was not created"
