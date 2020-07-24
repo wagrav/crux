235a6b321328db62f3109f68123d023c9dc68044
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-07-24 12:51:54 +0200

    refactors & tests

--- pipelines/azure/arm/create_cluster.sh
+++ pipelines/azure/arm/create_cluster.sh
@@ -11,8 +11,8 @@ create_cluster() {
   output=$(az deployment group create --name "$deployment_name" --resource-group "$resource_group" --template-file "$template_file" --parameters nodeSize="$node_size" nodeCount="$node_count" clusterNamePrefix="$cluster_name_prefix")
   cluster_name=$(echo $output |jq -r '.properties.outputs.name.value')
   echo "Cluster name created: $cluster_name"
-  echo "##vso[task.setvariable variable=$output_variable]${cluster_name}"
-  printf -v "$output_variable" "$cluster_name" #this is required because azure task will not set it for current script only subsequent steps
+  echo "##vso[task.setvariable variable=$output_variable]${cluster_name}" #set in pipeline
+  printf -v "$output_variable" "$cluster_name" #set in script, this is required because azure task will not set it for current script only subsequent steps
   echo "OUTPUT VAR: $output_variable"
   echo "OUTPUT VAR EXPANDED ${!output_variable}"
 }
