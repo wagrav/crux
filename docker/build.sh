ca7b7bae6ec299104e7bb596f953fc77b6594ad6
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-10 14:18:52 +0200

    Freeze crux to use particular docker image tagged 0.0.1

--- docker/build.sh
+++ docker/build.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
-function build() {
-  local image=$1
-  docker build -t $image -f Dockerfile .
-}
-
 function buildAll() {
   local group=$1
-  docker build --tag="$group/jmeter-base:latest" -f Dockerfile .
-  docker build --tag="$group/jmeter-master:latest" -f Dockerfile-master .
-  docker build --tag="$group/jmeter-slave:latest" -f Dockerfile-slave .
+  local prefix=$2
+  local tag=$3
+  docker build -t "$group/$prefix-base:$tag" -f Dockerfile .
+  docker build -t "$group/$prefix-master:$tag" -f Dockerfile-master .
+  docker build -t "$group/$prefix-slave:$tag" -f Dockerfile-slave .
 }
 function push(){
   local group=$1
+  local prefix=$2
+  local tag=$3
   docker login
-  docker push $group/jmeter-base
-  docker push $group/jmeter-master
-  docker push $group/jmeter-slave
+  docker push "$group/$prefix-base:$tag"
+  docker push "$group/$prefix-master:$tag"
+  docker push "$group/$prefix-slave:$tag"
 }
 #sh build.sh jmeter-chrome-selenium
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  buildAll gabrielstar && push gabrielstar
+  buildAll gabrielstar crux 0.0.1 && push gabrielstar crux 0.0.1
 fi
