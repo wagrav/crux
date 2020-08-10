44a45028ee84067e25049713698fc3131c1ffea5
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-10 17:08:49 +0200

    added code coverage

--- modules/pester/RunAllTests.ps1
+++ modules/pester/RunAllTests.ps1
@@ -5,8 +5,11 @@ $env=$args[0]
 "retrieve available modules"
 
 
-
-Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
-
+if($env -contains 'azure'){ #pester 5.X syntax
+    #workbook module, support code coverage
+    Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -CodeCoverageOutputFile $PSScriptRoot\results\workbook-pesterCoverageTEST.xml -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
+}else{ #local pester 3.4.0 syntax
+    Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
+}
 
 
