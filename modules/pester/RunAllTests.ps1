5152006c400b86d06c2e997b6c4b9a967816bc21
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-10 16:49:59 +0200

    chnaged order

--- modules/pester/RunAllTests.ps1
+++ modules/pester/RunAllTests.ps1
@@ -4,17 +4,18 @@ $env=$args[0]
 "script root: $PSScriptRoot"
 "retrieve available modules"
 
-#assures we all use teh same pester version
-if($env -contains 'azure'){
-    Uninstall-Module -Name Pester -Force  #uninstall default perester 5
-    Install-Module -Name Pester -Force -SkipPublisherCheck -RequiredVersion 3.4.0 #install windows 10 pester
-}else{
+
+if($env -contains 'azure'){ #pester 5.X syntax
+    #workbook module
+    Invoke-Pester -Script @{Path="$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1"} -CodeCoverage @{Path="$PSScriptRoot\..\workbooks\*.ps1"}  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
+
+}else{ #local pester 3.4.0 syntax
     $modules = Get-Module -list
     if ($modules.Name -notcontains 'pester') {
         Install-Module -Name Pester -Force -SkipPublisherCheck -RequiredVersion 3.4.0
     }
+    #workbook module
+    Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
 }
-#workbook module
-Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
 
 
