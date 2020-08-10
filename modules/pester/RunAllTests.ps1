2c4876ff58d468c7cb1877d64cbf53f023777943
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-10 16:14:49 +0200

    changed command to pester 5 syntax

--- modules/pester/RunAllTests.ps1
+++ modules/pester/RunAllTests.ps1
@@ -6,6 +6,7 @@ $modules = Get-Module -list
 if ($modules.Name -notcontains 'pester') {
     Install-Module -Name Pester -Force -SkipPublisherCheck
 }
+#workbook module
 Invoke-Pester -Script @{Path="$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1"} -CodeCoverage @{Path = "$PSScriptRoot\..\workbooks\*.ps1"; }  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
 
 
