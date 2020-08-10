252f3699c5a2b8f5399769cbb015badab1864c8b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-10 16:41:00 +0200

    added commands to use pester 3.4.0 on azure

--- modules/pester/RunAllTests.ps1
+++ modules/pester/RunAllTests.ps1
@@ -1,10 +1,18 @@
+$env=$args[0]
+"running on: $env"
 "current location: $(Get-Location)"
 "script root: $PSScriptRoot"
 "retrieve available modules"
 
-$modules = Get-Module -list
-if ($modules.Name -notcontains 'pester') {
-    Install-Module -Name Pester -Force -SkipPublisherCheck -RequiredVersion 3.4.0
+#assures we all use teh same pester version
+if($env -contains 'azure'){
+    Get-InstalledModule -Name Pester | Uninstall-Module -Force  #uninstall default perester 5
+    Install-Module -Name Pester -Force -SkipPublisherCheck -RequiredVersion 3.4.0 #install windows 10 pester
+}else{
+    $modules = Get-Module -list
+    if ($modules.Name -notcontains 'pester') {
+        Install-Module -Name Pester -Force -SkipPublisherCheck -RequiredVersion 3.4.0
+    }
 }
 #workbook module
 Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
