f94a00c8d2c37fe52dd7348a84d51338ee0555fd
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-10 16:57:41 +0200

    removed conditional block

--- modules/pester/RunAllTests.ps1
+++ modules/pester/RunAllTests.ps1
@@ -5,17 +5,8 @@ $env=$args[0]
 "retrieve available modules"
 
 
-if($env -contains 'azure'){ #pester 5.X syntax
-    #workbook module
-    Invoke-Pester -Script @{Path="$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1"} -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
 
-}else{ #local pester 3.4.0 syntax
-    $modules = Get-Module -list
-    if ($modules.Name -notcontains 'pester') {
-        Install-Module -Name Pester -Force -SkipPublisherCheck -RequiredVersion 3.4.0
-    }
-    #workbook module
-    Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
-}
+Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -CodeCoverage "$PSScriptRoot\..\workbooks\*.ps1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
+
 
 
