86364e3976192c5d32908e60c1537723e8e66692
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-17 12:34:31 +0200

    fixed scaling bug

--- kubernetes/bin/jmeter.sh
+++ kubernetes/bin/jmeter.sh
@@ -34,8 +34,8 @@ getPods(){
 copyDataToPods(){
   for pod in "${pods_array[@]}"; do
         echo "Copying contents of data dir to pod : $pod"
-        kubectl cp  "$root_dir/$data_dir" -n $tenant "$pod:/$test_dir/"
-        kubectl exec -ti -n $tenant $pod -- bash -c "cp -r /$test_dir/$data_dir/* /$test_dir/" #unpack to /test
+        kubectl cp  "$root_dir/$data_dir" -n $tenant "$pod:$test_dir/"
+        kubectl exec -ti -n $tenant $pod -- bash -c "cp -r $test_dir/$data_dir/* $test_dir/" #unpack to /test
         echo "Pod contents at /$test_dir:"
         kubectl exec -ti -n $tenant $pod -- ls "/$test_dir/"
   done
