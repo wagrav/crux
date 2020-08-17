269324573ab29eaa6e469cd0d1bdfc83aa6eedf6
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-17 12:59:58 +0200

    fixed paths

--- kubernetes/bin/jmeter.sh
+++ kubernetes/bin/jmeter.sh
@@ -37,10 +37,10 @@ copyDataToPods(){
         echo "Copying contents of data dir to pod : $pod"
         echo "$root_dir/$data_dir -n $tenant $pod:$test_dir/"
         kubectl cp  "$root_dir/$data_dir" -n $tenant "$pod:$test_dir/"
-        echo "Unpackign data on pod : $pod"
+        echo "Unpacking data on pod : $pod"
         echo "cp -r $test_dir/$data_dir_relative/* $test_dir/"
-        kubectl exec -ti -n $tenant $pod -- bash -c "cp -r $test_dir/$data_dir_relative/* $test_dir/" #unpack to /test
-        echo "Pod contents at /$test_dir:"
+        kubectl exec -ti -n $tenant $pod -- bash -c "cp -r $test_dir/test_data/* $test_dir/" #unpack to /test
+        echo "Pod contents at $test_dir:"
         kubectl exec -ti -n $tenant $pod -- ls "/$test_dir/"
   done
 }
