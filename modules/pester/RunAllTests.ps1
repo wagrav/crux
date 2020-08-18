fb903b2bf54c1d7dacf5263b8f6057ba1af739c5
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-08-18 09:56:49 +0200

    Fixing pester call

--- modules/pester/RunAllTests.ps1
+++ modules/pester/RunAllTests.ps1
@@ -4,14 +4,7 @@ $env=$args[0]
 "script root: $PSScriptRoot"
 "retrieve available modules"
 
+Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -PassThru -CodeCoverageOutputFile $PSScriptRoot\results\workbook-pesterCoverageTEST.xml -CodeCoverage "$PSScriptRoot\..\workbooks\*.psm1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
 
-if($env -contains 'azure'){ #pester 5.X syntax
-    #workbook module, support code coverage
-    Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -PassThru -CodeCoverageOutputFile $PSScriptRoot\results\workbook-pesterCoverageTEST.xml -CodeCoverage "$PSScriptRoot\..\workbooks\*.psm1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
-}else{ #local pester 3.4.0 syntax
-    Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1" -PassThru -CodeCoverageOutputFile $PSScriptRoot\results\workbook-pesterCoverageTEST.xml -CodeCoverage "$PSScriptRoot\..\workbooks\*.psm1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
-
-    #Invoke-Pester -Script "$PSScriptRoot\..\workbooks\Workbooks.Tests.ps1"  -PassThru  -CodeCoverage "$PSScriptRoot\..\workbooks\*.psm1"  -OutputFile $PSScriptRoot\results\workbook-pesterTEST.xml -OutputFormat 'NUnitXML'
-}
 
 
