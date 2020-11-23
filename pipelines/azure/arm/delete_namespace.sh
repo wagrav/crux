3e67248f79d47300bcee3bbab2da462b975b58fb
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-11-23 13:39:18 +0100

    dev(dev): adding auto namespace creation

--- pipelines/azure/arm/delete_namespace.sh
+++ pipelines/azure/arm/delete_namespace.sh
@@ -3,7 +3,7 @@ deleteNamespace(){
   local cluster_namespace=$1
   if [ "$namespace" != "default" ];then
     echo "Deleting namespace $cluster_namespace"
-    kubectl delete namespace "$namespace"
+    kubectl delete namespace "$cluster_namespace"
   fi
 
 }
