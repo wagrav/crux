5e7994b97440a3c421924aca94525dc0005561c2
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-11-23 16:58:36 +0100

    dev(dev): adding timeout for deployment

--- pipelines/azure/arm/wait_for_pods.sh
+++ pipelines/azure/arm/wait_for_pods.sh
@@ -2,18 +2,26 @@
 #help functions
 
 wait_for_pod() {
-  service_replicas="0/1"
-  service_namespace=$1
-  service=$2
-  service_replicas_number=$3
-  sleep_time_s=$4
+  local service_replicas="0/1"
+  local service_namespace=$1
+  local service=$2
+  local service_replicas_number=$3
+  local sleep_time_s=$4
+  local timeout=$timout
   status="Terminating"
   printf "\nWait for service $service pods to be in Running status with interval $sleep_interval"
-  until [ "$status" == "Running" ]; do
+  local elapsed=0
+  until [[ "$status" == "Running" || elapsed -lt timeout ]]; do
     sleep $sleep_time_s
+    elapsed=$((sleep_time_s+elapsed))
     status=$(kubectl -n $service_namespace get po | grep $service | awk '{print $3}' | sort | uniq) # this will display also old pods until they are gone
     printf "\nService $service pods statuses: $(echo $status | xargs)"
   done
+
+  if [[ elapsed -ge timeout ]]; then
+        echo "##[error] Deployment timed out. Scheduling conditions not satisfied?"
+        echo "##vso[task.complete result=Failed;]DONE"
+  fi
 }
 
 wait_for_pods() {
@@ -23,7 +31,7 @@ wait_for_pods() {
   shift 3
   IFS=' ' read -r -a services <<<"$@"
   for service in "${services[@]}"; do
-    wait_for_pod $service_namespace $service $service_replicas_number $sleep_time_s
+    wait_for_pod $service_namespace $service $service_replicas_number $sleep_time_s $timout
   done
 
 }
@@ -51,6 +59,7 @@ wait_for_cluster_ready(){
   local service_master=$3
   local service_slave=$4
   local scale_up_replicas=$5
+  local timeout=$6
 
   local scale_up_replicas_master=1
   local scale_down_replicas=0
@@ -81,8 +90,8 @@ wait_for_cluster_ready(){
   kubectl scale -n "$cluster_namespace" --replicas="$scale_up_replicas_master" -f "$rootPath/$master_file"
   kubectl scale -n "$cluster_namespace" --replicas="$scale_up_replicas" -f "$rootPath/$slave_file"
 
-  wait_for_pods "$cluster_namespace" $scale_up_replicas_master $sleep_interval $service_master
-  wait_for_pods "$cluster_namespace" $scale_up_replicas $sleep_interval $service_slave
+  wait_for_pods "$cluster_namespace" $scale_up_replicas_master $sleep_interval $service_master $timeout
+  wait_for_pods "$cluster_namespace" $scale_up_replicas $sleep_interval $service_slave $timeout
   displayDeploymentCorrectnessStatus $cluster_namespace
 }
 
