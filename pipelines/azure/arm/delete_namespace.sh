59d24496095ef646bf56ce901332194d899ee7b4
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-11-24 09:57:33 +0100

    dev(dev): add no-wait

--- pipelines/azure/arm/delete_namespace.sh
+++ pipelines/azure/arm/delete_namespace.sh
@@ -3,7 +3,7 @@ deleteNamespace(){
   local cluster_namespace=$1
   if [ "$namespace" != "default" ];then
     echo "Deleting namespace $cluster_namespace"
-    kubectl delete namespace "$cluster_namespace" --no-wait
+    kubectl delete namespace "$cluster_namespace"
   fi
 
 }
