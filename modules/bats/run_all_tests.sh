1da579f2513436fd897e490836803c5cae6885cc
Gabriel Starczewski <38763186+gabrielstar@users.noreply.github.com>
2020-12-08 10:13:34 +0100

    Feature/group steps by domain (#10)
    * dev(feature): remove dangling jmx file
    
    * dev(feature): remove dangling folders
    
    * dev(feature): remove dangling folders
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    * dev(feature): moving files
    
    Co-authored-by: Gabriel Starczewski <starczewski@objectivity.co.uk>

--- modules/bats/run_all_tests.sh
+++ modules/bats/run_all_tests.sh
@@ -5,7 +5,7 @@ function runModuleTests(){
  local module=$1
  local test_folder=$2
  printf "\n# Running tests for module $module\n"
- cd ../$module && bats --formatter junit *.bats --output "$test_folder"
+ cd ../$module && bats -r . --formatter junit --output "$test_folder"
  cd "$here"
 }
 
@@ -13,7 +13,7 @@ function runAll(){
   result_folder=$(pwd)/tmp
   mkdir -p "$result_folder"
   runModuleTests junit "$result_folder"
-  runModuleTests ../pipelines/azure/arm "$result_folder"
+  runModuleTests ../azure "$result_folder"
   runModuleTests ../kubernetes/bin "$result_folder"
 }
 
