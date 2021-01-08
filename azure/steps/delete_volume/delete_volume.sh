ec93921031711d0475853c710e6304b30e5f777b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-01-08 14:07:07 +0100

    dev(feature): helm pipeline step

--- azure/steps/delete_volume/delete_volume.sh
+++ azure/steps/delete_volume/delete_volume.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 delete_volume() { #public: deletes PVC
   local _cluster_namespace=$1
+  echo "Volumes:"
+  kubectl get pv --namespace "$_cluster_namespace"
   local _pv=$(kubectl get pv --namespace "$_cluster_namespace" -o=jsonpath='{.items[0].metadata.name}')
   kubectl delete "pv/$_pv"
 }
