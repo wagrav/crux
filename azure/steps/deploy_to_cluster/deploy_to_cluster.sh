b0a4af3c566cd08aa5726272f8974282209ab7dc
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-01-08 15:19:18 +0100

    dev(feature): helm pipeline step

--- azure/steps/deploy_to_cluster/deploy_to_cluster.sh
+++ azure/steps/deploy_to_cluster/deploy_to_cluster.sh
@@ -1,39 +1,6 @@
 #!/bin/bash
 #help functions
 
-_wait_for_pod() { #public: wait for pods of a given type to be in Running state
-  local _service_replicas="0/1"
-  local _service_namespace=$1
-  local _service=$2
-  local _service_replicas_number=$3
-  local _sleep_time_s=$4
-  local _status="Terminating"
-  echo "Wait for service $_service pods to be in Running status with interval $_sleep_time_s"
-  until [[ "$_status" == "Running" ]]; do
-    sleep "$_sleep_time_s"
-    _status="$( \
-      kubectl -n "$_service_namespace" get po \
-      | grep "$_service" \
-      | awk '{print $3}' \
-      | sort \
-      | uniq)" # this will display also old pods until they are gone
-    echo "Service $_service pods statuses: $(echo "$_status" | xargs)"
-  done
-
-}
-
-_wait_for_pods() { #public: waits for pods to be running for a list of services
-  local _service_namespace=$1
-  local _service_replicas_number=$2
-  local _sleep_time_s=$3
-  shift 3
-  IFS=' ' read -r -a _services <<<"$@"
-  for _service in "${_services[@]}"; do
-    _wait_for_pod "$_service_namespace" "$_service" "$_service_replicas_number" "$_sleep_time_s"
-  done
-
-}
-
 _replace_aks_pool_name(){
   local _aks_pool=$1
   local _root_path=$2
@@ -81,8 +48,6 @@ deploy_to_cluster() {
   echo "Scale up master to $_scale_up_replicas_master and slaves to $_scale_up_replicas_slave"
   kubectl scale -n "$_cluster_namespace" --replicas="$_scale_up_replicas_master" -f "$_root_path/$_jmeter_master_deploy_file"
   kubectl scale -n "$_cluster_namespace" --replicas="$_scale_up_replicas_slave" -f "$_root_path/$_jmeter_slaves_deploy_file"
-  _wait_for_pods "$_cluster_namespace" "$_scale_up_replicas_master" $_sleep_interval "$_service_master"
-  _wait_for_pods "$_cluster_namespace" "$_scale_up_replicas_slave" $_sleep_interval "$_service_slave"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
