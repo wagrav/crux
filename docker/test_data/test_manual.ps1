a6f171f181e9066c5e7420436ccea0e710e795d4
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-01-27 15:54:47 +0100

    dev(feature): moved to python3

--- docker/test_data/test_manual.ps1
+++
@@ -1,8 +0,0 @@
- #confirm container has headless chrome
- #RUN on windows in powershell
- docker run -v C:\Users\gstarczewski\repos\performance\IBR\docker\test_data:/test --shm-size=2g --rm -it gabrielstar/jmeter-master python /test/test.py
- #get inside the container
- docker run --shm-size=2g --rm -it gabrielstar/jmeter-master bash
- #then run jmeter tests with selenium and webdriver subsamples
- jmeter -Jwebdriver.sampleresult_class=com.googlecode.jmeter.plugins.webdriver.sampler.SampleResultWithSubs -n -l result.csv -t selenium_test_chrome_headless.jmx
-
